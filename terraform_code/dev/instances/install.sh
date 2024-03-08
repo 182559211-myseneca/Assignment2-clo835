a97bfa2f95f4f4d8266fcc9062b4826f3450ffda
182559211-myseneca <149635895+182559211-myseneca@users.noreply.github.com>
2024-03-07 19:49:58 -0500

    Delete terraform_code/dev/instances directory

--- terraform_code/dev/instances/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    sudo yum update -y
-    sudo yum install docker -y
-    sudo yum install git -y
-    sudo yum install mysql -y
-    sudo systemctl start docker
-    sudo usermod -a -G docker ec2-user
-    docker network create npaul19
